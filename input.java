d5845c0da52ea4468dce065bd01a3ffeb710db5a
maitreya-22 <maitreyapanjabi2208@gmail.com>
2022-01-13 16:28:03 +0530

    scanner class commit
    commit

--- input.java
+++ input.java
@@ -1,8 +1,8 @@
 import java.util.*;  
-class UserInputDemo   
+public class input
 {  
 public static void main(String[] args)  
-{  
+    {  
 Scanner sc= new Scanner(System.in);    //System.in is a standard input stream  
 System.out.print("Enter first number- ");  
 int a= sc.nextInt();  
--- test.java
+++ test.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class test{
     public static void main(String[] args) {
-        System.out.println("first commit ");
+        //System.out.println(args[0]);
+        Scanner input = new Scanner(System.in);
+
+        int a=Integer.parseInt(input.nextLine());
+        String str=input.nextLine();
+        System.out.println(a);
+        System.out.println(str);
+
      }
 }
+
+//public - class can be  accessed from anywhere
+
+//static - 
+
+//String[] args - command-line argument (array of strings)

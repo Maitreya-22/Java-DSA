bb7fa9c484605bd25f5b650de32a1c4d7420cb46
maitreya-22 <maitreyapanjabi2208@gmail.com>
2022-01-13 17:43:26 +0530

    added more code

--- test.java
+++ test.java
@@ -9,12 +9,24 @@ public class test{
         String str=input.nextLine();
         System.out.println(a);
         System.out.println(str);
+        //type casting
+        int num=(int) (57.76f);
+        System.out.println(num);
+        float z=257f;
+        byte b=(byte) (z);
+        System.out.println(b);
+        int number='A';
+        System.out.println(number);
 
      }
 }
+
+
 
 //public - class can be  accessed from anywhere
 
 //static - 
 
 //String[] args - command-line argument (array of strings)
+
+
